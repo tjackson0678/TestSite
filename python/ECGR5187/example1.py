6e597cc5c599db5826d8a88802b4f58aa38cc8ff
T. Jackson <tdj1191@yahoo.com>
2024-12-20 15:38:22 -0500

    testing

--- python/ECGR5187/example1.py
+++ python/ECGR5187/example1.py
@@ -5,6 +5,7 @@ x = ['06:00','10:00','14:00','18:00','20:00']
 y1 =  [12.949,13.125,30.162,12.698,12.733]
 y2 =  [21.560,22.794,22.273,21.868,153.594]
 
+
 plt.plot(x,y1, color='blue', label='West (stanford.edu)')
 plt.plot(x,y2, color='red', label='East (unc.edu)')
 plt.ylabel('Avg RTT (ms)')
@@ -12,5 +13,5 @@ plt.xlabel('Time of Day')
 plt.title("UNC and Stanford RTT")
 
 plt.legend()
-plt.savefig("ECGR5187_FinalProj.pdf")
+plt.savefig("/Users/tjacks5/TestSite/python/ECGR5187/ECGR5187_FinalProj5.pdf")
 plt.show()
