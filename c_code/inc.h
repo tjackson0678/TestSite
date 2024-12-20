62c63d915ec3b9a83f9e1cc0188599f057981529
T. Jackson <tdj1191@yahoo.com>
2024-12-20 12:58:43 -0500

    Adding some code from school

---
+++ c_code/inc.h
@@ -0,0 +1,3 @@
+#define MAX 5
+extern x[];
+extern int fill(); 
---
+++ c_code/sub.c
@@ -0,0 +1,11 @@
+#include "inc.h"
+
+int fill () {
+	int i = 0;
+	int x[9];
+
+	for (i=0; i<MAX; i++) {
+		x[i] = -1; 
+	}
+	return MAX; 
+}
